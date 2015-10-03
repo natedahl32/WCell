b89ad42ad2b834c5048f1cee245aabb5f539cdda
natedahl32 <nndahl@gmail.com>
2015-10-03 11:02:44 -0500

    Fix for durotar quest

---
+++ Addons/WCell.DefaultAddon/Quests/DurotarQuests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WCell.Core.Initialization;
+using WCell.RealmServer.Quests;
+
+namespace WCell.Addons.Default.Quests
+{
+    public static class DurotarQuests
+    {
+        static uint CUTTING_TEETH_QUESTION_ID = 788;
+
+        [Initialization]
+        [DependentInitialization(typeof(QuestMgr))]
+        public static void FixIt()
+        {
+            // Remove bad requirement
+            var cuttingTeeth = QuestMgr.GetTemplate(CUTTING_TEETH_QUESTION_ID);
+            cuttingTeeth.ReqAllFinishedQuests.Remove(787);
+        }
+    }
+}
